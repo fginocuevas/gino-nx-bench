5a4f2a344e481848a2884377f46e2046c8504d6b
Gino Cuevas <thenogicode@gmail.com>
2023-07-03 15:16:32 +0800

    Initial commit

---
+++ apps/gino-nx-bench/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'gino-nx-bench',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/gino-nx-bench',
+};
