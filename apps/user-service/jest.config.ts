1110276f1f744f850f99a83a11fa5cf72ab66c53
Gino <gino@old.st>
2023-07-03 15:39:49 +0800

    initial develop commit

--- apps/user-service/jest.config.ts
+++ apps/user-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'gino-nx-bench',
+  displayName: 'user-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/gino-nx-bench',
+  coverageDirectory: '../../coverage/apps/user-service',
 };
