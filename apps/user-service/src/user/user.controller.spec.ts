3295b734e8d7c6233682b485f7a84ff7bf13131e
Gino <gino@old.st>
2023-07-14 11:27:34 +0800

    feat: added prisma in user-service

--- apps/user-service/src/user/user.controller.spec.ts
+++ apps/user-service/src/user/user.controller.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaLibService } from '@prisma-lib';
+import { CoreLibModule } from '@core-lib';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports:[CoreLibModule],
       controllers: [UserController],
-      providers: [UserService],
+      providers: [UserService, PrismaLibService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
